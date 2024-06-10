786b6b7727eba6fdbefe46e3bfbb8f42aca03e16
Chris <christophergruca@richmedia.com>
2024-06-10 15:31:23 -0400

    ngd kids demo site

--- demos/ngd_kids/script.js
+++ demos/ngd_kids/script.js
@@ -1,16 +1,29 @@
-const items = document.querySelectorAll('.acc button');
+const items = document.querySelectorAll(".acc button");
 
 //accordion controls for mobile
 function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
+  const itemToggle = this.getAttribute("aria-expanded");
 
   for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
+    items[i].setAttribute("aria-expanded", "false");
   }
 
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
   }
 }
 
-items.forEach((item) => item.addEventListener('click', toggleAccordion));
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
+
+var modal = document.getElementById("warningModal");
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
